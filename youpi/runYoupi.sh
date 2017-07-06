bea18deddc20b908cddee10382641548e6d1068f
Colin González <colin.gonzalez@openmailbox.org>
2017-07-06 18:27:47 +0200

    Empty commit

--- youpi/runYoupi.sh
+++ youpi/runYoupi.sh
@@ -2,5 +2,6 @@ CONTRIB=$1
 URL=$2
 REPORT=$CONTRIB.csv
 echo $CONTRIB
+echo $CONTRIB > $REPORT
 docker build -t youpi .
-docker run -t --rm --name youpi-$CONTRIB youpi $URL > ../report/$REPORT
+docker run -t --rm --name youpi-$CONTRIB youpi $URL $CONTRIB > ../report/$REPORT
