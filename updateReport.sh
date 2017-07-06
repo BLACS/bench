a7ab45aa1c93fcf19ac15ed76af6e56e0d2ecf96
Colin González <colin.gonzalez@openmailbox.org>
2017-07-06 18:28:48 +0200

    Fixes report generation

--- updateReport.sh
+++ updateReport.sh
@@ -2,6 +2,22 @@
 
 echo "Implementation, Reading 10 Elements, Writing 100 Elements, Writing/Reading1000 Elements" > ../results/data.csv
 
+echo -n "Implementation, " >  header.csv
+
+cd youpi/requests/
+
+for test in write/*.rq; do echo  -n $test", "   ; done >> ../../header.csv
+
+for test in read/*.rq; do echo  -n $test", "   ; done >>  ../../header.csv
+
+for test in time/*.rq; do echo  -n $test", "   ; done >> ../../header.csv
+
+cd ../../
+
+cat header.csv | sed 's/..$//'  > ../results/data.csv
+
+echo "" >> ../results/data.csv
+
 for report in report/*; do cat $report >> ../results/data.csv; done
 
 cd ../results/
