d58c24d5b3f1ba659839850bdc0e2dd1f17c2c75
Colin González <colin.gonzalez@openmailbox.org>
2017-07-10 17:20:51 +0200

    Rewrite of check.sh to work with new layout

--- check.sh
+++ check.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 CONTRIB=$1
-./runDocker.sh $CONTRIB
+
+docker build -t $CONTRIB contrib/$CONTRIB/
+
+docker run -d --rm --name $CONTRIB $CONTRIB
+
+IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTRIB`
+IP=http://$IP
+
+echo $IP
 
 cd youpi
 
-./runYoupi.sh
+./runYoupi.sh $CONTRB $IP:8080
 
-./stopDocker.sh $CONTRIB
+
+
